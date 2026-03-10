9075d09f02d785984075d6e38604376a0d98162c
Carlitogames-cityboy <carlos.pinango@escola.pr.gov.br>
2026-03-10 11:41:24 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("Endereço Completo");
+let rua = "Rua das Flores";
+let numero = 911;
+let cidade = "Angrybird";
+
+let endereco = rua + "," + numero + "," + cidade;
+//console.log(endereco);
+console.log("O endereçõ correto é: " + endereco);
