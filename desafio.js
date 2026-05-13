aeebd2fea3c97d1123ec2f52d82dfeda9497cc06
Carlitogames-cityboy <carlos.pinango@escola.pr.gov.br>
2026-05-13 13:55:37 +0000

    versao1

---
+++ desafio.js
@@ -0,0 +1,10 @@
+//1. Declarar 5 variiáveis para representar um funcionário
+//Fazer uma saída de forma interpolada (`${}`)
+
+const nome = "CarlosD";
+let idade = 67;
+let cargo = "Gerente";
+let salario = 13.500;
+let empresa = "City Boys Corporation";
+
+console.log();
