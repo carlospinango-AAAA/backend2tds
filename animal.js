e9729641c9476a35c7427712e5050a374dc6331b
Carlitogames-cityboy <carlos.pinango@escola.pr.gov.br>
2026-06-03 13:53:39 +0000

    v1

---
+++ animal.js
@@ -0,0 +1,10 @@
+const animal = {
+    nome: "PinguencioMartinez", //string
+    especie: "PingassaurioREX",
+    idade: 67, //number
+    tutor: false,
+    vacinado: true
+};
+
+//console.log(animal);
+console.log(animal.nome);
