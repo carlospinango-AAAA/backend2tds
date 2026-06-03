2fb5fb971ae09f35754db37baf3e6c9b290becc1
Carlitogames-cityboy <carlos.pinango@escola.pr.gov.br>
2026-06-03 13:41:49 +0000

    4

--- desafio.js
+++ desafio.js
@@ -1,25 +1,19 @@
-//1. Declarar 5 variiáveis para representar um funcionário
-//Fazer uma saída de forma interpolada (`${}`)
-
-const nomeDoFuncionario = "CarlosD";
-let idade = 67;
+const nomeFuncionario = "CarlosD";
+let idade = "41";
+let anosEmpresa = 5;
 let cargo = "Gerente";
+let salario = "R$ 67.758";
+let salarioBonus = "R$ 467.50";
+let desconto = 10
+let valorTotal = "R$ 535.258";
 let estaAtivo = true;
-let area = "marketing";
 
-const salariobase = 6300;
-const salariobonus = 67;
-let salariofinal = salariobase + salariobonus;
-let desconto = 10;
- 
+numero1 = 5;
+numero2 = 10;
 
-
-let empresa = "City Boys Corporation";
-
-console.log ("Essa é a empresa " + empresa);
-
-console.log ("Trabalhador: " + nomeDoFuncionario);
-console.log ("Idade: " + idade);
-console.log (` Ele é o ${cargo} da area de ${area}`);
-console.log ("Salário: " + salariobase);
-console.log ("Salario bonus: " + salariobonus);
+console.log("       --Sobre o Funcionário--"); 
+console.log(`Nome: ${nomeFuncionario}     | Cargo: ${cargo}`);
+console.log(`Ìdade: ${idade}              | Anos de Empresa: ${anosEmpresa}`);
+console.log(`Salário Base: ${salario} | Bônus: ${salarioBonus} `);
+console.log(`Desconto: ${desconto}%`)
+console.log(`Valor líquido total: ${valorTotal}`);
