0c06fc62d9ca5256647942d025903748900db099
Carlitogames-cityboy <carlos.pinango@escola.pr.gov.br>
2026-06-08 13:49:15 +0000

    v1

--- animal.js
+++ animal.js
@@ -8,3 +8,10 @@ const animal = {
 
 //console.log(animal);
 console.log(animal.nome);
+console.log(animal.tutor);
+
+animal.idade = 6;
+console.log(animal);
+//console.log(animal);
+
+console.log(`${animal.nome} é um ${animal.especie} com a idade ${animal.idade} e possui sua vacina em dia ${animal.vacinado}`);
