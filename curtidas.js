c65280b4ca79d12329707d8ca52ea520af2b0866
Carlitogames-cityboy <carlos.pinango@escola.pr.gov.br>
2026-07-06 13:55:00 +0000

    v4

--- curtidas.js
+++ curtidas.js
@@ -2,8 +2,7 @@ const feedJSON = require('./feed.json');
 
 //Adicionar uma curtida ao post pelo id
 function curtir(feed, id) {
-    const post = feed[0].id === id ? feed[0]
-    }
+    const post = feed.find(item => item.id === id);
 
     if (!post) {
         console.log("Post não encontardo");
@@ -15,5 +14,21 @@ function curtir(feed, id) {
         return;
     }
 
-    post.metricas.curtidas++;
+    post.metricas.curtidas++; 
     console.log(`Post de @{post.autor} agora tem ${post.metricas.curtidas} curtidas.`);
+}
+
+//Remove uma curtida - não deixa ir abaixo de zero
+function descurtir(feed, id) {
+    const post = feed.find(item.id === id)
+
+if(post.metrica.curtidas === 0) {
+    console.log("Este post já está com curtidas.");
+    return;
+}
+
+post.metricas.curtidas++;
+console.log(`Curtida removida. Total ${post.metricas.curtidas}`);
+}
+
+curtir(feedJSON, 1);
